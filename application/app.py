f697a0c412f66d1ded4f2f18c10c06f259f2e89d
berkay <karatay.berkay@gmail.com>
2024-02-13 19:17:02 +0300

    mongo redis py app updated

--- application/app.py
+++ application/app.py
@@ -1,5 +1,10 @@
+import datetime
+import json
 import os
 import time
+
+import pymongo.collection
+
 from mongo_mixin import mongo_client
 
 
@@ -10,17 +15,34 @@ class App:
     def boot_application(self):
         path = os.getenv("PATH_INFO")
         method = os.getenv("REQUEST_METHOD")
-        print('path', path)
-        print('method', method)
-        print('routes', self.routes)
-        return {
-            "routes": self.routes
-        }
+        print("path", path)
+        print("method", method)
+        print("routes", self.routes)
+        return {"routes": self.routes}
 
 
 if __name__ == "__main__":
     app = App()
+    from pymongo.collection import Collection
     while True:
-        print('Mongo client initiated', mongo_client, type(mongo_client))
+        print("Mongo client initiated", mongo_client, type(mongo_client))
+        try:
+            database = mongo_client.mongo_database
+            collection: Collection = database['new_collection']
+            print("Database", database, type(database))
+            print(database.list_collection_names())
+            collection.insert_one({
+                "name": "new_entry",
+                "new_list": ["new", "list", "of", "values"],
+                "new_float": 3.14,
+                "new_int": 42,
+                "new_bool": True,
+                "new_dict": {"new_key": "new_value"},
+                "new_datetime": datetime.datetime.now(tz=datetime.timezone.utc).__str__()
+            })
+            result = collection.find({})
+            print("Result", list(result), ' | count : ', len(list(result)))
+        except Exception as e:
+            print("Error", e)
         app.boot_application()
-        time.sleep(5)
+        time.sleep(10)
--- application/mongo_mixin.py
+++ application/mongo_mixin.py
@@ -1,10 +1,7 @@
 from pymongo import MongoClient
 
 mongo_client = MongoClient(
-    username='mongo_user',
-    password='mongo_password',
-    host='mongo',
-    port=27017,
+    "mongodb://mongo_user:mongo_password@mongo:27017/mongo_database",
     document_class=dict,
-    connect=True
+    connect=True,
 )
